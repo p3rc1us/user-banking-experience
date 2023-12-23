2fd5791b068452f2f86a20c9eb60fc7ededccdf5
KERVY MANIEGO <kervymaniego@gmail.com>
2023-12-23 12:07:43 +0800

    transfer initialized

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,39 +1,46 @@
 import React from "react";
 import { useState } from 'react';
+import "../Styles/Dashboard.css"
 
 function Dashboard() {
 
-    const [Deposit, setDeposit] = useState('');
+    const [Transfer, setTransfer] = useState('');
     const [Balance, setBalance] = useState(2000);
 
 
-    function handleDeposit(e) {
+    function handleTransfer(e) {
         e.preventDefault();
 
-        const depositValue = parseInt(Deposit);
-        if (depositValue >= 0) {
-        setBalance(Balance + depositValue);
-        setDeposit("");
+        const transferValue = parseInt(Transfer);
+        if (transferValue >= 0) {
+            if (transferValue > Balance) {
+                alert(`Insufficient funds! You only have $${Balance.toLocaleString()}.`);
+                return;
         }
+
+        setBalance(Balance - transferValue);
+        setTransfer("");
+        }
+        
         else {
-            alert("please input right amount to deposit")
+            alert("please input right amount to transfer")
             return;
         }
     }
 
 return(
-    <div className="m-10 border-2 border-solid border-red-500">
-    <h1 className="text-6xl font-bold ">{Balance}</h1>
-    <form onSubmit={handleDeposit}>
+    <fieldset className="mainContainer">
+    <h1 className="text-6xl font-bold ">${Balance.toLocaleString()}</h1>
+    <form onSubmit={handleTransfer}>
     <input
           type="number"
-          value={Deposit}
+          value={Transfer}
           placeholder="$0.00"
-          onChange={(event) => setDeposit(event.target.value)}
+          onChange={(event) => setTransfer(event.target.value)}
     />
-    <button type="submit">submit</button>
+    <button type="submit">Transfer</button>
     </form>
-    </div>
+    </fieldset>
 )
 }
 
