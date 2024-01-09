784cc005470097d097343677364b77d3a1095a4f
KERVY MANIEGO <kervymaniego@gmail.com>
2024-01-09 17:46:27 +0800

    transfer already working with error handlers

--- src/Pages/Transfer.jsx
+++ src/Pages/Transfer.jsx
@@ -1,54 +1,57 @@
 import React from "react";
+import { useState } from "react";
+import { MyContext } from '../Components/Context';
+import { useContext } from 'react';
 
 function Deposit() {
-  return (
-    <h1>Transfer at your nearest branch</h1>
-)  
-}
 
+  const [ isAmountEntered, setIsAmountEntered ] = useState(false)
+  const { contextValue, updateContextValue } = useContext(MyContext);
+
+  const [ modifiedValue, setModifiedValue ] = useState('');
+  // const [ accountNumber, setAccountNumber ] = useState('');
+
+
+  const handleInputChange = (e) => {
+    setModifiedValue(e.target.value);
+  };
+
+  const handleModify = (e)  => {
+    e.preventDefault();
+
+    if((Number(modifiedValue) <= 0)){
+      alert("Please enter a positive number");
+      return;
+    }
+    if (Number(modifiedValue) < contextValue) {
+      setIsAmountEntered(true);
+      updateContextValue(contextValue - Number(modifiedValue));
+      alert(`$${Number(modifiedValue)} has  been transferred`)
+    } 
+    else {
+      alert("Transfer amount exceeds your balance");
+      return;
+    }
+    }
+
+  return (
+  <div className="flex justify-center items-center h-screen w-screen">
+    
+    {isAmountEntered ? null : (
+    <form
+    onSubmit={handleModify}>
+      <h1 className='text-5xl font-bold text-gray-800'>enter amount first</h1>
+      <input 
+      type="number" 
+      value={modifiedValue} 
+      onChange={handleInputChange}
+      onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}
+      />
+      <button type="submit">Submit</button>
+    </form>
+    )}
+    </div>
+  )
+}
 export default Deposit;
 
-// import { useState } from "react";
-// import TransactionChoices from "../Components/TransactionChoices";
-// //1231231231//
-// // import { MyContext } from '../Components/Context';
-// // import { useContext } from 'react';
-// //-------------//
-// function Deposit() {
-
-//   const [ isAmountEntered, setIsAmountEntered ] = useState(false)
-
-//   //123123123123//
-//   // const { contextValue, updateContextValue } = useContext(MyContext);
-//   const [modifiedValue, setModifiedValue] = useState(0);
-
-
-//   const handleInputChange = (e) => {
-//     setModifiedValue(e.target.value);
-//   };
-
-//   const handleModify = (e)  => {
-//     e.preventDefault();
-//     if (Number(modifiedValue) < 5000) {
-//       setIsAmountEntered(true);
-//       alert (`$${modifiedValue} is reserved for withdrawal`)
-//     } 
-//     else {
-//       alert("Deposit amount must be less than $5000");
-//     }
-//     }
-
-//   //------------------------------//
-    
-
-//   return (
-//   <div className="flex justify-center items-center h-screen w-screen">
-    
-//     {isAmountEntered ? null : (
-//     <form
-//     onSubmit={handleModify}>
-//       <h1 className='text-5xl font-bold text-gray-800'>enter amount first</h1>
-//       <input type="number" value={modifiedValue} onChange={handleInputChange} />
-//       <button type="submit">Submit</button>
-//     </form>
-//     )}
